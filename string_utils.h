e76988da1a6ce3b6712eecd1a75b6b6ab5685d8d
alzwded <alzwded@gmail.com>
2017-05-09 11:00:29 +0300

    convince windows to let me actually read unicode
    Interesting note: I've never noticed before, but a lot of text editors
    fail at reading UCS-2 text files for some reason. I thought this was the
    norm on windows?
    
    Anyway, we only support UTF-8 for now... helps keep things sane.

--- string_utils.h
+++ string_utils.h
@@ -3,15 +3,16 @@
 
 #include <string>
 #include <memory>
+#include <cstdio>
 
 std::wstring MB2W(const char* in, size_t length);
 std::wstring MB2W(const char* in);
 std::unique_ptr<char> W2MB(std::wstring const& in);
-#ifdef _MSC_VER
-# define wfopen _wfopen
-#else
-# include <cstdio>
-FILE* wfopen(const wchar_t*, const wchar_t*);
-#endif
+
+FILE* reopen_read_unicode(FILE*);
+FILE* open_read_unicode(const wchar_t*);
+FILE* open_write_binary(const wchar_t*);
+FILE* open_write_unicode(const wchar_t*);
+#define close_file(X) fclose((X));
 
 #endif
